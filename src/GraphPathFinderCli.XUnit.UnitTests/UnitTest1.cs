8e0d260c67169ee08aa22f25a55136035cb1eddf
Anthony Pitman <42813672+AnthonyPitman@users.noreply.github.com>
2025-05-28 23:45:41 -0400

    move project into src directory and adjust files

---
+++ src/GraphPathFinderCli.XUnit.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphPathFinderCli.XUnit.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Program p = new(42);
+        Assert.Equal(42, p.I);
+    }
+}
---
+++ src/GraphPathFinderCli/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace GraphPathFinderCli;
+
+public class Program
+{
+    public int I { get; set; }
+
+    public Program(int i)
+    {
+        I = i;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
